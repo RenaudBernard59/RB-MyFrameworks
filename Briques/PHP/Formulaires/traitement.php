0f4ba2486c431c23106a2d4d42ffaa5045084e93
RenaudBernard59 <renaud.bernard@p59production.com>
2017-10-09 15:46:14 +0200

    Upgrading form for PHP

--- Briques/PHP/Formulaires/traitement.php
+++ Briques/PHP/Formulaires/traitement.php
@@ -9,15 +9,15 @@ $_GET['lien']; //URL
 $_GET['phone']; //tel
 $_GET['age']; //INT[1-120]
 $_GET['position']; //INT 0, 20, 40, 60, 80, 100
-$_GET['couleur']; //
+$_GET['couleur']; // hexa
 $_GET['recherche']; //Texte
 ///////////////////////
-$_GET['birthdate']; //date jj/mm/aaaa
-$_GET['lever']; //time hh:mm
-$_GET['semaine']; // ss:aaaa
-$_GET['lemois']; // mm:aaaa
-$_GET['heureailleurs']; // jj/mm/aaaa hh:mm:ss
-$_GET['heurelocale']; // jj/mm/aaaa hh:mm:ss UMT+-X
+$_GET['birthdate']; //date renvoie aaa-mm-jj
+$_GET['lever']; //time hh:mm ou hh:mm:ss.nn
+$_GET['semaine']; // aaaaWss
+$_GET['lemois']; // aaa-mm
+$_GET['heureailleurs']; // aaa-mm-jj-Thh:mmZ ou aaa-mm-jj-Thh:mm:ss.nnZ
+$_GET['heurelocale']; // aaa-mm-jj-Thh:mm ou aaa-mm-jj-Thh:mm:ss.nn
 ///////////////////////
 $_GET[array('voyellea', 'voyellee', 'voyelley')]; //checked 1-n
 $_GET['voyelleunique']; //checked 1

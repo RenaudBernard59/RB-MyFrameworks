e840ba01ec4ec73cf4e2cccc6aa5aad130ef8d5d
RenaudBernard59 <renaud.bernard@p59production.com>
2017-10-09 16:38:41 +0200

    Make modification to form validator

--- Briques/PHP/Formulaires/traitement.php
+++ Briques/PHP/Formulaires/traitement.php
@@ -26,10 +26,79 @@ $_GET['france']; //checked 1
 $_GET['payscontinent']; //checked 1
 ///////////////////////
 
+//Filtres
+<?php
+
+$options = array(
+    'nom' => FILTER_SANITIZE_STRING,
+    'pass' => FILTER_SANITIZE_STRING,
+	'longtext' => FILTER_SANITIZE_STRING,
+    'mail' => FILTER_VALIDATE_EMAIL,
+	'lien' => FILTER_VALIDATE_URL,
+	'phone' => (?),
+	'position' => FILTER_VALIDATE_INT,
+	'couleur' => FILTER_FLAG_ALLOW_HEX,
+	'recherche' => (?), FILTER_SANITIZE_STRING,
+
+	'age' => (?),
+	'age' => (?),
+	'age' => (?),
+    'age' => (?), 
+	'age' => (?),
+	'age' => (?),
+	'age' => (?),
+	'age' => (?),
+	'age' => (?),
+    'age' => (?), 
+	'age' => (?),
+	'age' => (?),
+	'age' => (?),
+	'age' => (?),
+	'age' => (?),
+    'age' => (?), 
+	'age' => (?),
+	'age' => (?),
+	'age' => (?),
+	'age' => (?),
+	'age' => (?),
+    'age' => (?), 
+	'age' => (?),
+	'age' => (?),
+	'age' => (?),
+	'age' => (?),
+	'age' => (?),
+);
 
 
 
+function telValidator($numeroTelephone) {
 
+	function nettoyerNumero($numeroTelephone) {
+    //Enlever tous les caractères non numériques.
+    $numeroTelephone = preg_replace('`[^0-9]`', '', $numeroTelephone);
+    //Ajout de zéros si besoin.
+    $numeroTelephone = str_pad($numeroTelephone, 10, '0', STR_PAD_LEFT);
+    return $numeroTelephone;
+	}
+
+	var_dump(filter_var($numeroTelephone, FILTER_CALLBACK, array('options' => 'nettoyerNumero')));
+
+	function validerNumero($numberPhoneTest) {
+		//Retourne le numéro s'il est valide, sinon false.
+		return preg_match('`^0[1-9]([-. ]?[0-9]{2}){4}$`', $numberPhoneTest) ? $numberPhoneTest : false;
+	}
+	$filtreTel = array(
+		'numero' => array(
+		    'filter' => FILTER_CALLBACK,
+		    'options' => 'numberPhoneTest'
+
+		)
+	);
+	$resultatFiltreTel = filter_input_array(INPUT_GET, $filtreTel);
+	if($resultatFiltreTel['numeroTelephone'] !== false) {
+		echo 'Le numéro est valide.';
+	}
+}//END telValidator
 
 
 

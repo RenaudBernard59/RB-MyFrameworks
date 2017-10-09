95b8bbb13c44d3c20bd2d648882e81bfbb9221fe
RenaudBernard59 <renaud.bernard@p59production.com>
2017-10-09 15:37:20 +0200

    Preparing form for PHP

---
+++ Briques/PHP/Formulaires/traitement.php
@@ -0,0 +1,41 @@
+<?php
+///////////////////////
+$_GET['nom']; //Texte
+$_GET['pass']; //Texte
+$_GET['longtext']; //Texte
+///////////////////////
+$_GET['mail']; //Mail
+$_GET['lien']; //URL
+$_GET['phone']; //tel
+$_GET['age']; //INT[1-120]
+$_GET['position']; //INT 0, 20, 40, 60, 80, 100
+$_GET['couleur']; //
+$_GET['recherche']; //Texte
+///////////////////////
+$_GET['birthdate']; //date jj/mm/aaaa
+$_GET['lever']; //time hh:mm
+$_GET['semaine']; // ss:aaaa
+$_GET['lemois']; // mm:aaaa
+$_GET['heureailleurs']; // jj/mm/aaaa hh:mm:ss
+$_GET['heurelocale']; // jj/mm/aaaa hh:mm:ss UMT+-X
+///////////////////////
+$_GET[array('voyellea', 'voyellee', 'voyelley')]; //checked 1-n
+$_GET['voyelleunique']; //checked 1
+$_GET['pays']; //checked 1
+$_GET['france']; //checked 1
+$_GET['payscontinent']; //checked 1
+///////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
